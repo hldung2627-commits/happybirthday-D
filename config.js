6fafd82741c9f402da1a3af9490c5c08bb77dcd8
hldung2627-commits <hldung2627@gmail.com>
2026-08-29 00:20:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,13 +11,13 @@ const CONFIG = {
    memories: [
         { 
             year: "2023", 
-            images: ["20231.jpg", "20232.jpg", "20233.jpg", "20234.jpg", "20235.jpg", "20236.jpg", "20237.jpg", "20238.jpg"], 
+            images: ["20231.jpg", "20232.jpg", "20233.jpg", "20234.jpg", "20235.jpg", "20236.jpg", "20237.jpg", "20238.jpg", "20239.jpg"], 
             title: "Năm 2023", 
             description: "Cùng nhau trưởng thành, cùng nhau chia sẻ mọi khoảnh khắc vui buồn." 
         },
         { 
             year: "2024", 
-            images: ["20241.jpg", "20242.jpg", "20243.jpg", "20244.jpg", "20245.jpg", "20246.jpg", "20247.jpg", "20248.jpg"], 
+            images: ["20241.jpg", "20242.jpg", "20243.jpg", "20244.jpg", "20245.jpg", "20246.jpg", "20247.jpg", "20248.jpg", "20249.jpg", "202410.jpg", "202411.jpg", "202412.jpg", "202413.jpg", "202414.jpg", "202415.jpg"], 
             title: "Năm 2024", 
             description: "Bình yên đơn giản là những lúc được ở cạnh nhau thế này." 
         },
@@ -29,7 +29,7 @@ const CONFIG = {
         },
         { 
             year: "2026", 
-            images: ["20261.jpg", "20262.jpg", "20263.jpg", "20264.jpg", "20265.jpg", "20266.jpg", "20267.jpg", "20268.jpg"], 
+            images: ["20261.jpg", "20262.jpg", "20263.jpg", "20264.jpg", "20265.jpg", "20266.jpg", "20267.jpg", "20268.jpg", "20269.jpg", "202610.jpg", "202611.jpg", "202613.jpg"], 
             title: "Năm 2026", 
             description: "Chúc mừng sinh nhật! Tuổi mới hãy luôn rực rỡ và hạnh phúc nhé 💜" 
         }
